8439f83222ce32b5bdec1577a58ff5c48ef06354
A01702962 <isaac@DESKTOP-DDUJF11.localdomain>
2021-10-01 17:23:14 -0500

    Cambios del profe añadidos fork-join

--- examples/04-java-threads/Example11.java
+++ examples/04-java-threads/Example11.java
@@ -1,12 +1,11 @@
 // =================================================================
 //
 // File: Example11.java
-// Author(s): Sandra Tello A01703658 Isaac Planter A01702962
+// Author(s):
 // Description: This file implements the code that transforms a
 //				grayscale image. The time this implementation takes will
 //				be used as the basis to calculate the improvement obtained
 // 				with parallel technologies.
-//Usage: java Example11 ../images/lenna.png
 //
 // Copyright (c) 2020 by Tecnologico de Monterrey.
 // All Rights Reserved. May be reproduced for any non-commercial
@@ -19,27 +18,36 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
+public class Example11 extends Thread{
+	// place your code here
+	private static final int BLUR_WINDOW = 15;
+	private int src[], dest[], width, height, start, end;
 
-public class Example11 {
-	private int src[], dest[], width, height;
-
-	public Example11(int src[], int dest[], int width, int height) {
+	public Example11(int src[], int dest[], int width, int height, int start, int end) {
 		this.src = src;
 		this.dest = dest;
 		this.width = width;
 		this.height = height;
+		this.start = start;
+		this.end = end;
 	}
 
-	// palce your code here
-	private void grayPixel(int ren, int col) {
-		int i, j, pixel, dpixel;;
+	private void greyPixel(int ren, int col) {
+		int  i, j;
+		int pixel, dpixel;
 		float r, g, b, aux;
+
+		
+		r = 0; g = 0; b = 0;
+		
 		
 		pixel = src[(ren * width) + col];
-		r = (float) ((pixel & 0x00ff0000) >> 16);
-		g = (float) ((pixel & 0x0000ff00) >> 8);
-		b = (float) ((pixel & 0x000000ff) >> 0);
+
+		r += (float) ((pixel & 0x00ff0000) >> 16);
+		g += (float) ((pixel & 0x0000ff00) >> 8);
+		b += (float) ((pixel & 0x000000ff) >> 0);
 		aux = (r+g+b)/3;
+		
 
 		dpixel = (0xff000000)
 				| (((int) (aux)) << 16)
@@ -48,22 +56,21 @@ public class Example11 {
 		dest[(ren * width) + col] = dpixel;
 	}
 
-	void doTask() {
-		int index, size;
-		int ren, col;
+	public void run() {
+		int index, ren, col;
 
-		size = width * height;
-		for (index = 0; index < size; index++) {
+		for (index = start; index < end; index++) {
 			ren = index / width;
 			col = index % width;
-			grayPixel(ren, col);
+			greyPixel(ren, col);
 		}
-		// place your code here.
 	}
 
 	public static void main(String args[]) throws Exception {
 		long startTime, stopTime;
 		double ms;
+		Example11 threads[];
+		int src[], dest[], w, h, block;
 
 		if (args.length != 1) {
 			System.out.println("usage: java Example11 image_file");
@@ -74,22 +81,40 @@ public class Example11 {
 		File srcFile = new File(fileName);
         final BufferedImage source = ImageIO.read(srcFile);
 
-		int w = source.getWidth();
-		int h = source.getHeight();
-		int src[] = source.getRGB(0, 0, w, h, null, 0, w);
-		int dest[] = new int[src.length];
+		w = source.getWidth();
+		h = source.getHeight();
+		src = source.getRGB(0, 0, w, h, null, 0, w);
+		dest = new int[src.length];
 
-		System.out.printf("Starting...\n");
+		block = (w * h) / Utils.MAXTHREADS;
+		threads = new Example11[Utils.MAXTHREADS];
+
+		System.out.printf("Starting with %d threads...\n", Utils.MAXTHREADS);
 		ms = 0;
-		Example11 e = new Example11(src, dest, w, h);
-		for (int i = 0; i < Utils.N; i++) {
+		for (int j = 1; j <= Utils.N; j++) {
+			for (int i = 0; i < threads.length; i++) {
+				if (i != threads.length - 1) {
+					threads[i] = new Example11(src, dest, w, h,
+										(i * block), ((i + 1) * block));
+				} else {
+					threads[i] = new Example11(src, dest, w, h,
+										(i * block), (w * h));
+				}
+			}
+
 			startTime = System.currentTimeMillis();
-
-			e.doTask();
-
+			for (int i = 0; i < threads.length; i++) {
+				threads[i].start();
+			}
+			for (int i = 0; i < threads.length; i++) {
+				try {
+					threads[i].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 			stopTime = System.currentTimeMillis();
-
-			ms += (stopTime - startTime);
+			ms +=  (stopTime - startTime);
 		}
 		System.out.printf("avg time = %.5f\n", (ms / Utils.N));
 		final BufferedImage destination = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
@@ -103,7 +128,7 @@ public class Example11 {
 
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-               ImageFrame.showImage("Gray - " + fileName, destination);
+               ImageFrame.showImage("Blur - " + fileName, destination);
             }
         });*/
 		try {
@@ -112,6 +137,5 @@ public class Example11 {
 		} catch (IOException ioe) {
 			System.out.println("Exception occured :" + ioe.getMessage());
 		}
-
 	}
 }
--- examples/05-fork-join/Example4.java
+++ examples/05-fork-join/Example4.java
@@ -14,8 +14,69 @@ import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.ForkJoinPool;
 
 public class Example4 extends RecursiveTask<Integer> {
-	private static final int SIZE = 100_000_000;
+	private static final int SIZE = 100_000_00;
 	private static final int MIN = 100_000;
+	//HOLA
+	private int array[], start, end, result;
 
-	// place your code here
+	public Example4(int array[], int start, int end) {
+		this.array = array;
+		this.start = start;
+		this.end = end;
+		this.result = 0;
+	}
+
+	public Integer computeDirectly() {
+		result = 0;
+		for(int i=0; i<array.length; i++){
+			if(array[i]%2==0){
+				result++;
+			}
+		}
+		return result;
+	}
+
+	@Override
+	protected Integer compute() {
+		if ( (end - start) <= MIN ) {
+			return computeDirectly();
+		} else {
+			int mid = start + ( (end - start) / 2 );
+			Example4 lowerMid = new Example4(array, start, mid);
+			lowerMid.fork();
+			Example4 upperMid = new Example4(array, mid, end);
+			return ((int) Math.min(upperMid.compute(), lowerMid.join()));
+		}
+	}
+
+	public static void main(String args[]) {
+		long startTime, stopTime;
+		int array[], result = 0;
+		double ms;
+		ForkJoinPool pool;
+
+		array = new int[SIZE];
+		Utils.fillArray(array);
+		Utils.displayArray("array", array);
+
+		int pos = Math.abs(Utils.r.nextInt()) % SIZE;
+		System.out.printf("Setting value 0 at %d\n", pos);
+		array[pos] = 0;
+
+		System.out.printf("Starting with %d threads...\n", Utils.MAXTHREADS);
+		ms = 0;
+		for (int i = 0; i < Utils.N; i++) {
+			startTime = System.currentTimeMillis();
+
+			pool = new ForkJoinPool(Utils.MAXTHREADS);
+			result = pool.invoke(new Example4(array, 0, array.length));
+
+			stopTime = System.currentTimeMillis();
+			ms += (stopTime - startTime);
+		}
+		System.out.printf("result = %d\n", result);
+		System.out.printf("avg time = %.5f\n", (ms / Utils.N));
+	}
 }
+
+
