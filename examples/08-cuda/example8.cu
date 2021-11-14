4d8a9512f5c3c7f80a3d169da9a894c1bf6c24b5
A01702962 <A01702962@itesm.mx>
2021-11-14 10:25:31 -0600

    cambios en el 8.1

--- examples/08-cuda/example8.cu
+++ examples/08-cuda/example8.cu
@@ -21,9 +21,8 @@
 #define THREADS 256
 #define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
 
-__global__ void enumerationSort(int*arr, int size){
+__global__ void enumerationSort(int*arr, int*aux, int size){
 
-   int aux[size];
    int tid = threadIdx.x + (blockIdx.x * blockDim.x);
 
    while (tid < size) {
@@ -43,36 +42,46 @@ __global__ void enumerationSort(int*arr, int size){
 
 
 int main(int argc, char* argv[]) {
-	int i, j, *a, *d_a;
+	int i, j, *a, *aux *d_a, *d_aux;
 	double ms;
 
 	a = (int *) malloc(sizeof(int) * SIZE);
+    aux = (int *) malloc(sizeof(int) * SIZE);
     random_array(a, SIZE);
 	display_array("before", a);
     
     cudaMalloc( (void**) &d_a, SIZE * sizeof(int) );
+    cudaMalloc( (void**) &d_aux, SIZE * sizeof(int) );
 
     printf("Starting...\n");
 	ms = 0;
 	for (i = 1; i <= N; i++) {
         cudaMemcpy(d_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_aux, aux, SIZE * sizeof(int), cudaMemcpyHostToDevice);
 
 	    start_timer();
 
-        enumerationSort<<<1, THREADS>>>(d_a, SIZE);
+        enumerationSort<<<1, THREADS>>>(d_a, d_aux, SIZE);
+        for (int k = 0; k<SIZE; k++){
+			a[k] = aux[k];
+		}
 
 	    ms += stop_timer();
 	}
 
     cudaMemcpy(a, d_a, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(aux, d_aux, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+
     printf("avg time = %.5lf ms\n", (ms / N));
 
     display_array("after", a);
 
 
 	cudaFree(d_a);
+    cudaFree(d_aux);
 
 	free(a);
+    free(aux);
 
   return 0;
 }
